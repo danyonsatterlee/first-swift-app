f60e3d30457dd6978ee13eb9723349090932d923
Danyon Satterlee <danyon.satterlee@gmail.com>
2021-02-21 10:41:54 -0700

    hello world button

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func showMessage(sender:UIButton) {
+        let alertController = UIAlertController(title: "Welcome to my first app", message: "HelloWorld", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
